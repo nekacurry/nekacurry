5e8be1fa579944038e8b1341bf2229e7944e3b26
nekacurry <anneka498@gmail.com>
2022-12-06 16:56:26 -0600

    integrated chakraui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import { Container, Box } from "@chakra-ui/react"
+
+
 const Page = () => {
   return (
-    <div>Hello World</div>
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3}>
+        Hello, I'm a full stack developer based in Chicago, IL
+      </Box>
+      <div>Hello World</div>
+    </Container>
   )
 }
 
