09c2763d9f5262af5b7bb9767f38d13744c196cd
nekacurry <anneka498@gmail.com>
2022-12-06 16:31:05 -0600

    created index

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
-import { Box } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 
 const Main = ({ children, router}) => {
   return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content='width=device-width, initial-scale=1' />
+        <title>Anneka Curry - Home</title>
+      </Head>
 
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
   )
 }
+
+export default Main
