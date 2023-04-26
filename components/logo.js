d3f5526d1a45b45d7afd4eedffcb73d1cf1961c8
nekacurry <anneka498@gmail.com>
2023-04-26 11:54:44 -0500

    added font

--- components/logo.js
+++ components/logo.js
@@ -26,7 +26,7 @@ const Logo = () => {
         <Image src={eyeDoImg} width={20} height={20} alt="logo"/>
         <Text 
           color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-          fontFamily='M PLUS Rounded 1c'
+          fontFamily='Quicksand'
           fontWeight="bold"
           ml={2}>
             Anneka Curry
