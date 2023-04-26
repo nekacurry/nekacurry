c1cd8225adccd7bc5d05b0d6bc9347cca82188a4
nekacurry <anneka498@gmail.com>
2023-04-26 12:26:37 -0500

    added profile image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="indianred" p={3} mb={6} align="center">
-        Hiya! I'm a Midwestern full stack developer
+      <Box 
+        borderRadius="lg" 
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
+        p={3} 
+        mb={6} 
+        mt={1}
+        align="center"
+      >
+        Hi there! I'm a Midwestern Full-Stack Developer.
       </Box>
       
       <Box display={{md: 'flex'}}>
@@ -13,7 +20,23 @@ const Page = () => {
           <Heading as="h2" variant="page-title">
             Anneka Curry
           </Heading> 
-          {/* <p>Advanced Media Creation Girl</p> */}
+          <p>Media Magical Girl ( Developer // Artist // Content Creator ) </p>
+        </Box>
+        <Box 
+          flexShrink={0} 
+          mt={{base: 4, md: 0}} 
+          ml={{md: 6}} 
+          align="center"
+        >
+          <Image 
+            borderColor="whiteAlpha.800" 
+            borderWidth={2} 
+            borderStyle="solid"  
+            maxWidth="100px" 
+            display="inline-block" 
+            borderRadius="full" 
+            src="/images/anneka.png"
+            alt="Profile Image"/>
         </Box>
       </Box>
     </Container>
