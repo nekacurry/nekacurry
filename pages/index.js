13d8f28ca90eeff549b2992609812af2aac70ef3
nekacurry <anneka498@gmail.com>
2023-04-26 13:12:41 -0500

    added bio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
-
+import { Button, Container, Box, Heading, Image, useColorModeValue, Link } from "@chakra-ui/react"
+import Section from "../components/section"
+import Paragraph from "../components/paragraph"
+import { ChevronRightIcon } from "@chakra-ui/icons"
+import NextLink from 'next/link'
+import { BioSection, BioYear } from "../components/bio"
 
 const Page = () => {
   return (
@@ -39,6 +43,46 @@ const Page = () => {
             alt="Profile Image"/>
         </Box>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Anneka is a 2023 grad and full-stack developer based in the Midwestern U.S. with a passion for all things creative. 
+          She has worked to launch multiple products from design to development, and has a knack for problem-solving with code.
+          Whether it be beautiful, responsive websites, graphic design, or streaming, Anneka uses her dedication to bring a fresh look to any project.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1998</BioYear>
+          Born in Chicago, Illinois
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Began Applied Computer Science Program @ 
+          <Link href="https://makeschool.org/" target="_blank"> Make School</Link>
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Continued ACS Program @ Dominican University of California
+        </BioSection>
+        <BioSection>
+          <BioYear>2023</BioYear>
+          Graduated from Dominican University of California with a B.S. in Applied Computer Science
+        </BioSection>
+      </Section>
     </Container>
   )
 }
